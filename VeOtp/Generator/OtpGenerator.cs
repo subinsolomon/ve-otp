9e668605aeb33309e78b416559e8ddb7ee88b714
thomasmichaelwallace <thomasmichaelwallace@gmail.com>
2016-08-11 11:12:32 +0100

    Outlined otp generation class for generation spec.

---
+++ VeOtp/Generator/OtpGenerator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ve.Otp.Generator
+{
+    public class OtpGenerator
+    {
+        public string generate(string userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
